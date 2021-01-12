baaaffa07828ff1cce3d5dc39e3eec9c3d78b2d0
alain2020 <jeremypanjaitan@gmail.com>
2021-01-12 11:52:10 +0700

    last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { MessageBox } from "react-chat-elements";
+import { Input } from "react-chat-elements";
+import socketIOClient from "socket.io-client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const ENDPOINT = "http://127.0.0.1:5000/";
+    const [userData, setUserData] = useState({
+        username: "",
+        room: "",
+    });
+    const [message, setMessage] = useState("");
+    const [socket, setSocket] = useState(null);
+    const [messageList, setMessageList] = useState([]);
+    const handleSendMessage = (socket, message) => {
+        socket.emit("SEND_MESSAGE", {
+            message,
+            from: userData.username,
+        });
+    };
+    useEffect(() => {
+        const listener = (e) => {
+            if (e.key === "Enter") {
+                handleSendMessage(socket,message);
+            }
+        }
+        document.addEventListener("keyup", listener);
+        return () => {
+            document.removeEventListener("keyup",listener);
+        };
+    }, [socket, message]);
+    useEffect(() => {
+        if (userData.username && userData.room) {
+            const socket = socketIOClient(ENDPOINT);
+            setSocket(socket);
+        } else {
+            const username = prompt("enter username");
+            const room = prompt("enter room");
+
+            setUserData({ username, room });
+        }
+    }, [userData]);
+    useEffect(() => {
+        if (socket) {
+            socket.emit("JOIN", userData);
+            socket.on("USER_JOINED", (message) => {
+                alert(message);
+            });
+        }
+    }, [socket]);
+    useEffect(() => {
+        if (socket) {
+            socket.on("RELAY_MESSAGE", (message) => {
+                console.log(messageList);
+                setMessageList([...messageList, message]);
+                setMessage("");
+            });
+        }
+    }, [socket, messageList]);
+    const messageBoxList = messageList.map((d) => {
+        return (
+            <MessageBox
+                position={d.from === userData.username ? "right" : "left"}
+                type={"text"}
+                text={d.message}
+                title={d.from === userData.username ? "" : d.from}
+            />
+        );
+    });
+    return (
+        <div className="app">
+            {socket && (
+                <div className="chat">
+                    <div className="chat__header">
+                        <h2>{userData.username}</h2>
+                        <h2>{userData.room}</h2>
+                    </div>
+                    <div className="chat__messageList">{messageBoxList}</div>
+                    <div className="chat__inputMessage">
+                        <textarea
+                            type="text"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                        <button
+                            onClick={() => {
+                                handleSendMessage(socket, message);
+                            }}
+                        >
+                            Send
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
