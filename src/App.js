08a970d96c0be1f56097656518c2b0e814fcb5c2
alain2020 <jeremypanjaitan@gmail.com>
2021-01-13 12:33:57 +0700

    last commit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,65 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useReducer } from "react";
 import { MessageBox } from "react-chat-elements";
 import { Input } from "react-chat-elements";
 import socketIOClient from "socket.io-client";
 
+const initialState = { messages: [] };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "RECEIVE_MESSAGE":
+            return { messages: [...state.messages, action.message] };
+        default:
+            throw new Error();
+    }
+};
+
 const App = () => {
     const ENDPOINT = "http://127.0.0.1:5000/";
-    const [userData, setUserData] = useState({
-        username: "",
-        room: "",
-    });
-    const [message, setMessage] = useState("");
     const [socket, setSocket] = useState(null);
-    const [messageList, setMessageList] = useState([]);
-    const handleSendMessage = (socket, message) => {
-        socket.emit("SEND_MESSAGE", {
-            message,
-            from: userData.username,
-        });
+    const textRef = useRef();
+    const chatContainerBottomRef = useRef();
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const [userData, setUserData] = useState(null);
+
+    const handleSendMessage = (socket, message, from) => {
+        socket.emit("SEND_MESSAGE", { from, message });
+        textRef.current.value = "";
     };
+
     useEffect(() => {
+        const socket = socketIOClient(ENDPOINT);
+        setSocket(socket);
+        const username = prompt("username");
+        const room = prompt("room");
+        setUserData({ username, room });
+
+        socket.emit("JOIN", { username, room });
+        socket.on("RELAY_MESSAGE", (message) => {
+            
+            chatContainerBottomRef.current.scrollIntoView({ behavior: 'smooth' });
+            dispatch({ type: "RECEIVE_MESSAGE", message });
+        });
         const listener = (e) => {
             if (e.key === "Enter") {
-                handleSendMessage(socket,message);
+                handleSendMessage(socket, textRef.current.value, username);
             }
-        }
-        document.addEventListener("keyup", listener);
-        return () => {
-            document.removeEventListener("keyup",listener);
         };
-    }, [socket, message]);
-    useEffect(() => {
-        if (userData.username && userData.room) {
-            const socket = socketIOClient(ENDPOINT);
-            setSocket(socket);
-        } else {
-            const username = prompt("enter username");
-            const room = prompt("enter room");
+        document.addEventListener("keyup", listener);
 
-            setUserData({ username, room });
-        }
-    }, [userData]);
-    useEffect(() => {
-        if (socket) {
-            socket.emit("JOIN", userData);
-            socket.on("USER_JOINED", (message) => {
-                alert(message);
-            });
-        }
-    }, [socket]);
-    useEffect(() => {
-        if (socket) {
-            socket.on("RELAY_MESSAGE", (message) => {
-                console.log(messageList);
-                setMessageList([...messageList, message]);
-                setMessage("");
-            });
-        }
-    }, [socket, messageList]);
-    const messageBoxList = messageList.map((d) => {
+        return () => {
+            document.removeEventListener("keyup", listener);
+        };
+    }, []);
+    const messageBoxList = state.messages.map((d) => {
         return (
+            
             <MessageBox
                 position={d.from === userData.username ? "right" : "left"}
+                title={d.from === userData.username ? "" : d.from}
                 type={"text"}
                 text={d.message}
-                title={d.from === userData.username ? "" : d.from}
             />
         );
     });
@@ -76,17 +71,20 @@ const App = () => {
                         <h2>{userData.username}</h2>
                         <h2>{userData.room}</h2>
                     </div>
-                    <div className="chat__messageList">{messageBoxList}</div>
+                    <div className="chat__messageList">
+                        {messageBoxList}
+                        <div className="chat__bottom" ref={chatContainerBottomRef}></div>
+                    </div>
                     <div className="chat__inputMessage">
-                        <textarea
-                            type="text"
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
-                        />
+                        <textarea type="text" ref={textRef} />
                         <button
-                            onClick={() => {
-                                handleSendMessage(socket, message);
-                            }}
+                            onClick={() =>
+                                handleSendMessage(
+                                    socket,
+                                    textRef.current.value,
+                                    userData.username
+                                )
+                            }
                         >
                             Send
                         </button>
